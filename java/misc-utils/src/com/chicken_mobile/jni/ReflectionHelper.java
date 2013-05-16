1c420b05d2ea6ef1bd4469d58e4e80e16bc1e082
Hugo Arregui <hugo.arregui@gmail.com>
2013-05-16 19:57:34 -0300

    bug fixing
    - ##sys#pointer->string uses get-object-class/jni and a local ref
      (thanks Mercora)
    - handling superclass methods
    - jimport module name is hygienic

--- java/misc-utils/src/com/chicken_mobile/jni/ReflectionHelper.java
+++ java/misc-utils/src/com/chicken_mobile/jni/ReflectionHelper.java
@@ -3,12 +3,36 @@ package com.chicken_mobile.jni;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ReflectionHelper {
 	
-	public static Method[] findMethods(Class<?> c, String name) {
-		ArrayList<Method> methods = new ArrayList<Method>();
+	public static Field[] findFields(Class<?> c) {
+		Set<Field> fields = new HashSet<Field>();
+		for (Field m : c.getFields()) {
+			fields.add(m);
+		}
+		for (Field m : c.getDeclaredFields()) {
+			fields.add(m);
+		}
+		return fields.toArray(new Field[fields.size()]);
+	}
+	
+	public static Method[] findMethods(Class<?> c) {
+		Set<Method> methods = new HashSet<Method>();
+		for (Method m : c.getMethods()) {
+			methods.add(m);
+		}
 		for (Method m : c.getDeclaredMethods()) {
+			methods.add(m);
+		}
+		return methods.toArray(new Method[methods.size()]);
+	}
+	
+	public static Method[] findMethodsByName(Class<?> c, String name) {
+		ArrayList<Method> methods = new ArrayList<Method>();
+		for (Method m : ReflectionHelper.findMethods(c)) {
 			if (m.getName().equals(name)) {
 				methods.add(m);
 			}
