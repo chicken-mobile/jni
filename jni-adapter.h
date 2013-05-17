34376717cebca98a06a6d40cfd0ba81bcf066045
Patrick Brauer (mercora) <mercora@LiLeth.NET>
2013-05-17 06:18:46 +0000

    allow arbitary jvm options to be set

--- jni-adapter.h
+++ jni-adapter.h
@@ -1,15 +1,25 @@
 #include <jni.h>
 
-jint jvm_create(JavaVM** jvm, void** jenv, char* classpath, char* stack)
-{
-    JavaVMInitArgs vm_args;
-    JavaVMOption options[2];
-    options[0].optionString = classpath;
-    options[1].optionString = stack;
-    vm_args.version = JNI_VERSION_1_6;
-    vm_args.options = options;
-    vm_args.nOptions = 2;
-
-    return JNI_CreateJavaVM(jvm, jenv, &vm_args);
+char* string_to_pointer(char* string){
+  printf(string, "\n");
+  return string;
 }
 
+jint jvm_create(JavaVM** jvm, void** jenv, void** pointerVector, uint length)
+{
+  JavaVMInitArgs vm_args;
+  JavaVMOption options[length];
+
+  vm_args.version = JNI_VERSION_1_6;
+
+  static int i;
+  for(i = 0; i < length; i++){
+    options[i].optionString = *(((char**)pointerVector) + i);
+  }
+
+  vm_args.options = options;
+  vm_args.nOptions = length;
+
+
+  return JNI_CreateJavaVM(jvm, jenv, &vm_args);
+}
