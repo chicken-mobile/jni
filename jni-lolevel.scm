ce61b535ca7a1e1532ec982927ed9b540528dcdc
Hugo Arregui <hugo.arregui@gmail.com>
2013-05-17 14:16:08 -0300

    Revert "allow arbitary jvm options to be set"
    This reverts commit 34376717cebca98a06a6d40cfd0ba81bcf066045.

--- jni-lolevel.scm
+++ jni-lolevel.scm
@@ -7,86 +7,45 @@
 <#
 
 (module jni-lolevel
-*
-(import chicken scheme foreign srfi-1 data-structures extras moremacros)
-(import-for-syntax chicken numbers scheme)
-(use lolevel foreigners srfi-13 srfi-1 data-structures numbers)
+        *
+        (import chicken scheme foreign srfi-1 data-structures extras moremacros)
+        (import-for-syntax chicken numbers scheme)
+        (use lolevel foreigners srfi-13 srfi-1 data-structures numbers)
 
-(include "jni-primitives.scm")
-(include "jni-defs.scm")
-(include "jni-jlambda.scm")
-(include "jni-reflection.scm")
+        (include "jni-primitives.scm")
+        (include "jni-defs.scm")
+        (include "jni-jlambda.scm")
+        (include "jni-reflection.scm")
 
-(cond-expand
- (android)
- (else
-  (define-foreign-record-type (jvm-option "JavaVMOption")
-    (constructor: make-jvm-option)
-    (destructor: free-jvm-option)
-    (c-string  optionString jvm-option-string jvm-option-string-set!)
-    ((c-pointer void) extraInfo jvm-option-info jvm-option-info-set!))
+        (cond-expand 
+          (android)
+          (else
+            (define-foreign-record-type (jvm-option "JavaVMOption")
+              (constructor: make-jvm-option)
+              (destructor: free-jvm-option)
+              (c-string  optionString jvm-option-string jvm-option-string-set!)
+              ((c-pointer void) extraInfo jvm-option-info jvm-option-info-set!))
 
-  (define-foreign-record-type (jvm-init-args "JavaVMInitArgs")
-    (constructor: make-jvm-init-args)
-    (destructor: free-jvm-init-args)
-    (jint version jvm-init-args-version jvm-init-args-version-set!)
-    (jint nOptions jvm-init-args-options-length jvm-init-args-options-length-set!)
-    (jvm-option options jvm-init-args-options jvm-init-args-options-set!)
-    (jboolean ignoreUnrecognized jvm-init-args-options-ignore-unrecognized
-	      jvm-init-args-options-ignore-unrecognized-set!))
+            (define-foreign-record-type (jvm-init-args "JavaVMInitArgs")
+              (constructor: make-jvm-init-args)
+              (destructor: free-jvm-init-args)
+              (jint version jvm-init-args-version jvm-init-args-version-set!)
+              (jint nOptions jvm-init-args-options-length jvm-init-args-options-length-set!)
+              (jvm-option options jvm-init-args-options jvm-init-args-options-set!)
+              (jboolean ignoreUnrecognized jvm-init-args-options-ignore-unrecognized 
+                        jvm-init-args-options-ignore-unrecognized-set!))
 
-  (define jvm-get-default-init-args
-    (foreign-lambda jint JNI_GetDefaultJavaVMInitArgs jvm-init-args))
+            (define jvm-get-default-init-args
+              (foreign-lambda jint JNI_GetDefaultJavaVMInitArgs jvm-init-args))
 
+            (define jvm-create
+              (foreign-lambda jint jvm_create (c-pointer java-vm) (c-pointer (c-pointer void)) c-string c-string))
 
-  (define (jvm-option prefix option vals)
-    (string-append
-     (or prefix "-")
-     (if (null? vals)
-	 option
-	 (let ((val (car vals)))
-	   (cond ((number? val)
-		  (string-append option
-				 (number->string val)
-				 (let ((unit (and (pair? (cdr vals)) (cadr vals))))
-				   (if unit (->string unit) ""))))
-		 ((boolean? val)
-		  (string-append (if val "+" "-") option))
-		 (else
-		  (string-append option "=" (->string val))))))))
-
-  (define (jvm-property-option property value)
-    (jvm-option "-D" (keyword->string property) (list value)))
-
-  (define (jvm-non-standard-option property . value)
-    (jvm-option "-X" (keyword->string property) value))
-
-  (define (jvm-hotspot-option option . value)
-    (jvm-option "-XX:" (keyword->string option) value))
-
-  (define string->pointer
-    (foreign-lambda (c-pointer char) string_to_pointer c-string))
-
-  (define jvm-create
-    (foreign-lambda jint jvm_create (c-pointer java-vm) (c-pointer (c-pointer void)) pointer-vector unsigned-int))
-
-  (define (jvm-init-lolevel #!optional (class-path ".") (stack-size 2) #!rest option-strings)
-    (let ((class-path-option (jvm-property-option     java.class.path: class-path))
-	  (stack-option      (jvm-non-standard-option ss:              stack-size 'M)))
-
-      (let* ((length (+ (length option-strings) 2))
-	     (pv (make-pointer-vector length)))
-
-	(fold (lambda (x i)
-		(let* ((string (->string x))
-		       (mem    (allocate (string-length string))))
-		  (move-memory! string mem)
-		  (pointer-vector-set! pv i mem) (+ i 1)))
-	      0 (cons class-path-option (cons stack-option (or option-strings '()))))
-
-
-	(let-location ((jvm java-vm)
-		       (env jni-env))
-	  (jvm-create (location jvm) (location env) pv length)
-	  (java-vm jvm)
-	  (jni-env env))))))))
+            (define (jvm-init-lolevel #!optional (class-path ".") (stack-size "2m"))
+              (let ((class-path-option (string-append "-Djava.class.path=" class-path))
+                    (stack-option      (string-append "-Xss" stack-size)))
+                (let-location ((jvm java-vm)
+                               (env jni-env))
+                              (jvm-create (location jvm) (location env) class-path-option stack-option)
+                              (java-vm jvm)
+                              (jni-env env)))))))
